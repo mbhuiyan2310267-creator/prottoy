c0ae6fa4356f07a4a10c3ae8a5352627ec847ffe
mbhuiyan2310267-creator <mbhuiyan2310267@bscse.uiu.ac.bd>
2026-07-16 19:35:31 +0600

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -84,5 +84,18 @@ const donors = [
     lastDonation: "",
     verified: false,
     demo: true
-  }
+  },
+    {
+    id: 1,
+    name: "Zobaid Rafi",
+    bloodGroup: "O+",
+    district: "কুমিল্লা",
+    upazila: "নাঙ্গলকোট",
+    Donation Area: "Dhaka",
+    phone: "01568249004",
+    available: true,
+    lastDonation: "2026-03-10",
+    verified: false,
+    demo: true
+  },
 ];
